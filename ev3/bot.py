ed5fea740a0018c7f20bfa0083101f84a60b86e6
Lowik CHANUSSOT <lowikchanussot@gmail.com>
2016-07-20 23:36:02 +0200

    Debugging Button
    Debugging Button
    
    Debugging Button
    
    Debugging Button
    
    Debugging Button
    
    Debugging Button

--- ev3/bot.py
+++ ev3/bot.py
@@ -4,20 +4,21 @@ import sys
 
 
 def wait_yes_or_no(timeout_s=5.0):
-    sleep_s = 0.5
+    sleep_s = 0.005
     yes_or_no = -1
 
+    button = ev3.Button()
     start = time.time()
     while yes_or_no < 0:
-        if ev3.Button.down:
+        if button.down:
             yes_or_no = 0
-        if ev3.Button.up:
+        if button.up:
             yes_or_no = 1
 
         if time.time() - start > timeout_s - sleep_s:
             return yes_or_no
         time.sleep(sleep_s)
-
+    return yes_or_no
 
 def main():
     ev3.Sound.speak("Hello Human. I'm a robot, do you want to play with me").wait()
@@ -25,7 +26,7 @@ def main():
     wait_user_answer = True
     n_trys = 0
     while wait_user_answer and n_trys < 2:
-        user_answer = wait_yes_or_no()
+        user_answer = wait_yes_or_no(10)
         if user_answer == 1:
             ev3.Sound.speak("Cool, let's play!").wait()
             wait_user_answer = False
@@ -38,8 +39,10 @@ def main():
 
     if n_trys > 2:
         ev3.Sound.speak("It seems you don't want to play. Bye!").wait()
+        time.sleep(5.0)
         sys.exit()
 
+    time.sleep(5.0)
 
 if __name__ == "__main__":
     main()
