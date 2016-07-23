eeee97b9fe520e71209b314bc114bc85d657370f
Lowik CHANUSSOT <lowikchanussot@gmail.com>
2016-07-23 17:27:07 +0200

    Add movement

--- ev3/RobotMove.py
+++ ev3/RobotMove.py
@@ -1,4 +1,5 @@
 import ev3dev.ev3 as ev3
+import time
 
 class RobotMove(object):
 
@@ -6,11 +7,14 @@ class RobotMove(object):
         self.left_wheel = ev3.Motor(ev3.OUTPUT_C)
         self.right_wheel = ev3.Motor(ev3.OUTPUT_B)
         self.head = ev3.Motor(ev3.OUTPUT_A)
+        self.left_wheel.reset()
+        self.right_wheel.reset()
+        self.head.reset()
 
     def forward(self, speed, distance_m):
-        time = 1.0
+        run_for_s = 1.0
         self.left_wheel.run_forever(duty_cycle_sp=speed)
         self.right_wheel.run_forever(duty_cycle_sp=speed)
-        time.sleep(time)
+        time.sleep(run_for_s)
         self.left_wheel.stop()
         self.right_wheel.stop()
